2042dcc9591da570a8fa00a2327aeaec0b5686b8
thekuwayama <thekuwayama@gmail.com>
2023-06-25 15:14:15 +0900

    add clap crate

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::process;
 
 use env_logger;
 use matter::core::CommissioningData;
@@ -6,23 +7,108 @@ use matter::data_model::cluster_basic_information::BasicInfoConfig;
 use matter::pairing::{print_pairing_code_and_qr, DiscoveryCapabilities};
 use matter::secure_channel::spake2p::VerifierData;
 
+mod cli;
+
 fn main() {
+    let matches = cli::build().get_matches();
+    let vid = matches
+        .get_one::<String>(cli::VENDOR_ID)
+        .unwrap()
+        .parse::<u16>()
+        .unwrap_or_else(|_| {
+            eprintln!("failed, <{}> should be integer", cli::VENDOR_ID);
+            process::exit(1);
+        });
+    let pid = matches
+        .get_one::<String>(cli::PRODUCT_ID)
+        .unwrap()
+        .parse::<u16>()
+        .unwrap_or_else(|_| {
+            eprintln!("failed, <{}> should be integer", cli::PRODUCT_ID);
+            process::exit(1);
+        });
+    let hw_ver = matches
+        .get_one::<String>(cli::HARDWARE_VERSION)
+        .unwrap()
+        .parse::<u16>()
+        .unwrap_or_else(|_| {
+            eprintln!("failed, <{}> should be integer", cli::HARDWARE_VERSION);
+            process::exit(1);
+        });
+    let sw_ver = matches
+        .get_one::<String>(cli::SOFTWARE_VERSION)
+        .unwrap()
+        .parse::<u32>()
+        .unwrap_or_else(|_| {
+            eprintln!("failed, <{}> should be integer", cli::SOFTWARE_VERSION);
+            process::exit(1);
+        });
+    let serial_no = matches
+        .get_one::<String>(cli::SERIAL_NUMBER)
+        .unwrap_or_else(|| {
+            eprintln!("failed, <{}> should be hex string", cli::SERIAL_NUMBER);
+            process::exit(1);
+        });
+    let device_name = matches
+        .get_one::<String>(cli::DEVICE_NAME)
+        .unwrap_or_else(|| {
+            eprintln!("failed, <{}> should be string", cli::DEVICE_NAME);
+            process::exit(1);
+        });
+    let verifier = matches
+        .get_one::<String>(cli::VERIFIER)
+        .unwrap()
+        .parse::<u32>()
+        .unwrap_or_else(|_| {
+            eprintln!("failed, <{}> should be integer", cli::VERIFIER);
+            process::exit(1);
+        });
+    let discriminator = matches
+        .get_one::<String>(cli::DISCRIMINATOR)
+        .unwrap()
+        .parse::<u16>()
+        .unwrap_or_else(|_| {
+            eprintln!("failed, <{}> should be integer", cli::DISCRIMINATOR);
+            process::exit(1);
+        });
+
+    do_print_pairing_code_and_qr(
+        vid,
+        pid,
+        hw_ver,
+        sw_ver,
+        serial_no.to_string(),
+        device_name.to_string(),
+        verifier,
+        discriminator,
+    );
+}
+
+fn do_print_pairing_code_and_qr(
+    vid: u16,
+    pid: u16,
+    hw_ver: u16,
+    sw_ver: u32,
+    serial_no: String,
+    device_name: String,
+    verifier: u32,
+    discriminator: u16,
+) {
     env::set_var("RUST_LOG", "info");
     env_logger::init();
 
     let dev_info = BasicInfoConfig {
-        vid: 0xFFF1,
-        pid: 0x8002,
-        hw_ver: 2,
-        sw_ver: 1,
-        sw_ver_str: "1".to_string(),
-        serial_no: "aabbccdd".to_string(),
-        device_name: "Smart Speaker".to_string(),
+        vid,
+        pid,
+        hw_ver,
+        sw_ver,
+        sw_ver_str: sw_ver.to_string(),
+        serial_no,
+        device_name,
     };
     let comm_data = CommissioningData {
-        // TODO: Hard-coded for now
-        verifier: VerifierData::new_with_pw(123456),
-        discriminator: 250,
+        verifier: VerifierData::new_with_pw(verifier),
+        discriminator,
     };
     let discovery_capabilities = DiscoveryCapabilities::new(true, true, true);
     print_pairing_code_and_qr(&dev_info, &comm_data, discovery_capabilities);
